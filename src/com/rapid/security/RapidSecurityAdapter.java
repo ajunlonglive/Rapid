2ec0f512196f2df3faaefa580a5138c0f5c30522
Gareth Edwards <gareth.edwards@rapid-is.co.uk>
2017-02-16 09:55:28 +0000

    Update to RapidSecurityAdapter to avoid duplicate users

--- src/com/rapid/security/RapidSecurityAdapter.java
+++ src/com/rapid/security/RapidSecurityAdapter.java
@@ -8,9 +8,9 @@ gareth.edwards@rapid-is.co.uk
 This file is part of the Rapid Application Platform
 
 Rapid is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as 
-published by the Free Software Foundation, either version 3 of the 
-License, or (at your option) any later version. The terms require you 
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version. The terms require you
 to include the original copyright, and the license notice in all redistributions.
 
 This program is distributed in the hope that it will be useful,
@@ -27,6 +27,7 @@ package com.rapid.security;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletContext;
@@ -46,48 +47,48 @@ import com.rapid.server.RapidRequest;
 import com.rapid.utils.Files;
 
 public class RapidSecurityAdapter extends SecurityAdapter {
-	
+
 	// security object which is marshalled and unmarshalled
-	
+
 	@XmlRootElement
 	public static class Security {
-		
+
 		private Roles _roles;
 		private Users _users;
-		
+
 		// properties (these are marshalled in and out of the security.xml file)
-		
+
 		public Roles getRoles() { return _roles; }
 		public void setRoles(Roles roles) { _roles = roles; }
-		
+
 		public Users getUsers() { return _users; }
 		public void setUsers(Users users) { _users = users; }
-		
+
 		// constructors
-		
+
 		public Security() {
 			_roles = new Roles();
 			_users = new Users();
 		}
-		
+
 	}
-						
+
 	// instance variables
-	
-	private static Logger _logger = LogManager.getLogger(RapidSecurityAdapter.class);	
+
+	private static Logger _logger = LogManager.getLogger(RapidSecurityAdapter.class);
 	private Marshaller _marshaller;
 	private Unmarshaller _unmarshaller;
 	private Security _security;
-			
+
 	// constructor
 
 	public RapidSecurityAdapter(ServletContext servletContext, Application application) {
 		// call the super method which retains the context and application
 		super(servletContext, application);
 		// init the marshaller and ununmarshaller
-		try { 
+		try {
 			JAXBContext jaxb = JAXBContext.newInstance(Security.class);
-			_marshaller = jaxb.createMarshaller(); 
+			_marshaller = jaxb.createMarshaller();
 			_marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 			_marshaller.setAdapter(RapidHttpServlet.getEncryptedXmlAdapter());
 			_unmarshaller = jaxb.createUnmarshaller();
@@ -98,38 +99,38 @@ public class RapidSecurityAdapter extends SecurityAdapter {
 		// load the users and roles
 		load();
 	}
-	
+
 	// instance methods
-	
+
 	private void load() {
-		
+
 		try {
-			
+
 			// create a file object for the security.xml file
 			File securityFile = new File(_application.getConfigFolder(_servletContext) + "/security.xml");
-			
+
 			// check the file exists
 			if (securityFile.exists()) {
-				
+
 				// unmarshal the xml into an object
 				_security = (Security) _unmarshaller.unmarshal(securityFile);
-									
+
 			} else {
-				
+
 				// initialise the security object
 				_security = new Security();
-				
+
 			}
-			
+
 			// initialise roles if necessary
 			if (_security.getRoles() == null) _security.setRoles(new Roles());
-			
+
 			// initialise users if necessary
 			if (_security.getUsers() == null) _security.setUsers(new Users());
-			
+
 			// retain whether we added users or roles to the security object
 			boolean modified = false;
-			
+
 			// some old versions of rapid did not have roles with separate names and descriptions check them here
 			for (int i = 0; i < _security.getRoles().size(); i ++) {
 				// we're using a for loop as we will later the collection
@@ -144,7 +145,7 @@ public class RapidSecurityAdapter extends SecurityAdapter {
 					i--;
 				}
 			}
-			
+
 			// some old versions of rapid held the users in a hashtable since we are now using a list we need to remove invalid entries
 			for (int i = 0; i < _security.getUsers().size(); i ++) {
 				// we're using a for loop as we will later the collection
@@ -159,7 +160,7 @@ public class RapidSecurityAdapter extends SecurityAdapter {
 					i--;
 				}
 			}
-						
+
 			// assume we don't have the admin role
 			boolean gotAdminRole = false;
 			// assume we don't have the design role
@@ -184,96 +185,65 @@ public class RapidSecurityAdapter extends SecurityAdapter {
 				modified = true;
 			}
 
-			/*			 
-			
-			 17/05/2016 GE - admin/admin and user/user should not be added automatically to all new applications. Rapid action will add current user
-			 
-			// add default users if there aren't any
-			if (_security.getUsers().size() == 0) {
-				
-				// create a list of roles we want the new user to have
-				UserRoles adminUserRoles = new UserRoles();
-				adminUserRoles.add(Rapid.ADMIN_ROLE);
-				adminUserRoles.add(Rapid.DESIGN_ROLE);
-				
-				// initialise the an "admin" user with the roles above
-				User adminUser = new User("admin", "Admin user", "admin", adminUserRoles);
-				
-				// add the admin user
-				_security.getUsers().add(adminUser);
-				
-				// initialise a simple user with no roles
-				User simpleUser = new User("user", "Unprivileged user", "user");
-				
-				// add the simple user
-				_security.getUsers().add(simpleUser);
-				
-				// record that we modified
-				modified = true;
-				
-			}
-			
-			*/
-			
-			// save the files if we modified it							
+			// save the files if we modified it
 			if (modified) save();
-														    
+
 		} catch (Exception ex) {
 			_logger.error(ex);
 		}
-		
+
 	}
-	
+
 	private void save() {
-		
+
 		try {
-								
+
 			// create a file object for the security.xml file
 			File securityFile = new File(_application.getConfigFolder(_servletContext) + "/security.xml");
-	
+
 			// create a temp file for saving the application to
 			File tempFile = new File(_application.getConfigFolder(_servletContext) + "/security-saving.xml");
-			
+
 			// get a file output stream to write the data to
-			FileOutputStream fos = new FileOutputStream(tempFile.getAbsolutePath());		
-			
+			FileOutputStream fos = new FileOutputStream(tempFile.getAbsolutePath());
+
 			// marshal the security object to the temp file
 			_marshaller.marshal(_security, fos);
-				    
+
 			// close the stream
 		    fos.close();
-		    
-		    // copy / overwrite the app file with the temp file	    	    	    
+
+		    // copy / overwrite the app file with the temp file
 		    Files.copyFile(tempFile, securityFile);
-		    
+
 		    // delete the temp file
 		    tempFile.delete();
-	    
+
 		} catch (Exception ex) {
 			_logger.error(ex);
 		}
-	    
+
 	}
-	
-	
+
+
 	// overrides
-	
+
 	@Override
 	public Roles getRoles(RapidRequest rapidRequest) throws SecurityAdapaterException {
 		return _security.getRoles();
 	}
-	
+
 	@Override
 	public Users getUsers(RapidRequest rapidRequest) throws SecurityAdapaterException {
 		return _security.getUsers();
 	}
-	
+
 	@Override
 	public Role getRole(RapidRequest rapidRequest, String roleName)	throws SecurityAdapaterException {
 		for (Role role : _security.getRoles()) if (role.getName().toLowerCase().equals(roleName.toLowerCase())) return role;
 		return null;
 	}
-	
+
 	@Override
 	public User getUser(RapidRequest rapidRequest) throws SecurityAdapaterException {
 		for (User user : _security.getUsers()) {
@@ -283,7 +253,7 @@ public class RapidSecurityAdapter extends SecurityAdapter {
 		}
 		return null;
 	}
-			
+
 	@Override
 	public void addRole(RapidRequest rapidRequest, Role role) throws SecurityAdapaterException {
 		Roles roles = _security.getRoles();
@@ -291,49 +261,72 @@ public class RapidSecurityAdapter extends SecurityAdapter {
 		roles.sort();
 		save();
 	}
-	
+
 	@Override
-	public void deleteRole(RapidRequest rapidRequest, String roleName) throws SecurityAdapaterException {
+	public boolean deleteRole(RapidRequest rapidRequest, String roleName) throws SecurityAdapaterException {
 		Roles roles = _security.getRoles();
-		roles.remove(roleName);		
-		save();
+		boolean removed = roles.remove(roleName);
+		if (removed)	save();
+		return removed;
 	}
-	
+
 	@Override
 	public void addUser(RapidRequest rapidRequest, User user) throws SecurityAdapaterException {
-		_security.getUsers().add(user);
-		_security.getUsers().sort();
+		// get the list of users
+		Users users = _security.getUsers();
+		// get this user name
+		String userName = user.getName();
+		// users to remove - their may be more than one for legacy reasons
+		List<User> removeUsers = new ArrayList<User>();
+		// loop the roles returning true as soon as there's a match
+		for (User u : users) if (user.getName().equals(userName)) {
+			// remember to remove this user
+			removeUsers.add(u);
+		}
+		// if we are removing any users
+		if (removeUsers.size() > 0) {
+			// loop remove users
+			for (User u : removeUsers) users.remove(u);
+		}
+		users.add(user);
+		users.sort();
 		save();
 	}
-	
+
 	@Override
-	public void deleteUser(RapidRequest rapidRequest) throws SecurityAdapaterException {
+	public boolean deleteUser(RapidRequest rapidRequest) throws SecurityAdapaterException {
 		for (User user : _security.getUsers()) if (user.getName().equals(rapidRequest.getUserName())){
-			_security.getUsers().remove(user);
-			_security.getUsers().sort();
-			save();
-			break;
-		}			
+			boolean removed = _security.getUsers().remove(user);
+			if (removed) {
+				_security.getUsers().sort();
+				save();
+			}
+			return removed;
+		}
+		return false;
 	}
-	
+
 	@Override
 	public void addUserRole(RapidRequest rapidRequest, String roleName) throws SecurityAdapaterException {
 		User user = getUser(rapidRequest);
 		if (user == null) throw new SecurityAdapaterException("User " + rapidRequest.getUserName() + " cannot be found");
 		user.getRoles().add(roleName);
 		user.getRoles().sort();
-		save();		
-	}
-	
-	@Override
-	public void deleteUserRole(RapidRequest rapidRequest, String roleName) throws SecurityAdapaterException {
-		User user = getUser(rapidRequest);
-		if (user == null) throw new SecurityAdapaterException("User " + rapidRequest.getUserName() + " cannot be found");
-		user.getRoles().remove(roleName);
-		user.getRoles().sort();
 		save();
 	}
-				
+
+	@Override
+	public boolean deleteUserRole(RapidRequest rapidRequest, String roleName) throws SecurityAdapaterException {
+		User user = getUser(rapidRequest);
+		if (user == null) throw new SecurityAdapaterException("User " + rapidRequest.getUserName() + " cannot be found");
+		boolean removed = user.getRoles().remove(roleName);
+		if (removed) {
+			user.getRoles().sort();
+			save();
+		}
+		return removed;
+	}
+
 	@Override
 	public boolean checkUserRole(RapidRequest rapidRequest, String roleName) throws SecurityAdapaterException {
 		User user = getUser(rapidRequest);
@@ -342,7 +335,7 @@ public class RapidSecurityAdapter extends SecurityAdapter {
 		}
 		return false;
 	}
-	
+
 	@Override
 	public boolean checkUserRole(RapidRequest rapidRequest, List<String> roleNames) throws SecurityAdapaterException {
 		User user = getUser(rapidRequest);
@@ -353,7 +346,7 @@ public class RapidSecurityAdapter extends SecurityAdapter {
 		}
 		return false;
 	}
-	
+
 	@Override
 	public void updateRole(RapidRequest rapidRequest, Role role) throws SecurityAdapaterException {
 		Role currentRole = getRole(rapidRequest, role.getName());
@@ -361,15 +354,17 @@ public class RapidSecurityAdapter extends SecurityAdapter {
 		currentRole.setDescription(role.getDescription());
 		save();
 	}
-	
+
 	@Override
 	public void updateUser(RapidRequest rapidRequest, User user) throws SecurityAdapaterException {
-		deleteUser(rapidRequest);
-		_security.getUsers().add(user);
-		_security.getUsers().sort();
-		save();
+		// the form adapter will return true to any password check so it looks like any user is in the app, only save if delete returns true meaning user was really in the app
+		if (deleteUser(rapidRequest)) {
+			_security.getUsers().add(user);
+			_security.getUsers().sort();
+			save();
+		}
 	}
-				
+
 	@Override
 	public boolean checkUserPassword(RapidRequest rapidRequest, String userName, String password) throws SecurityAdapaterException {
 		User user = null;
@@ -397,5 +392,5 @@ public class RapidSecurityAdapter extends SecurityAdapter {
 		}
 		return false;
 	}
-						
+
 }
