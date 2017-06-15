9cac7c71b61624c4a5dbbb645f5860ed5efa9e9d
Gareth Edwards <gareth.edwards@rapid-is.co.uk>
2017-06-15 14:20:58 +0100

    Fix for SQL webservices datetime

--- src/com/rapid/soa/SQLWebservice.java
+++ src/com/rapid/soa/SQLWebservice.java
@@ -8,9 +8,9 @@ gareth.edwards@rapid-is.co.uk
 This file is part of the Rapid Application Platform
 
 Rapid is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as 
-published by the Free Software Foundation, either version 3 of the 
-License, or (at your option) any later version. The terms require you 
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version. The terms require you
 to include the original copyright, and the license notice in all redistributions.
 
 This program is distributed in the hope that it will be useful,
@@ -27,6 +27,7 @@ package com.rapid.soa;
 
 import java.sql.Date;
 import java.sql.ResultSet;
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 
 import com.rapid.core.Application;
@@ -38,143 +39,146 @@ import com.rapid.server.RapidRequest;
 import com.rapid.soa.SOASchema.SOASchemaElement;
 
 public class SQLWebservice extends Webservice {
-		
+
 	// private variables
-	
+
 	private int _databaseConnectionIndex = -1;
 	private String _sql;
-		
+
 	// properties
-	
+
 	public int getDatabaseConnectionIndex() { return _databaseConnectionIndex; }
 	public void setDatabaseConnectionIndex(int databaseConnectionIndex) { _databaseConnectionIndex = databaseConnectionIndex; }
-	
+
 	public String getSQL() { return _sql; }
 	public void setSQL(String sql) { _sql = sql; }
-	
+
 	// constructors
-		
+
 	// used by Jaxb
 	public SQLWebservice() {}
 	// used by Rapid action
 	public SQLWebservice(String name) {
 		setName(name);
 	}
-	
+
 	// overrides
-	
+
 	@Override
 	public SOAData getResponseData(RapidRequest rapidRequest, SOAData requestData) throws WebserviceException {
-		
+
 		try {
-			
+
 			SimpleDateFormat dateFormatter = null;
-			
+
 			Date date = null;
-			
+
 			Application application = rapidRequest.getApplication();
-												
+
 			DatabaseConnection databaseConnection = application.getDatabaseConnections().get(_databaseConnectionIndex);
-			
-			ConnectionAdapter ca = databaseConnection.getConnectionAdapter(rapidRequest.getRapidServlet().getServletContext(), application);			
-			
+
+			ConnectionAdapter ca = databaseConnection.getConnectionAdapter(rapidRequest.getRapidServlet().getServletContext(), application);
+
 			DataFactory df = new DataFactory(ca);
-			
+
 			Parameters parameters = new Parameters();
-			
+
 			SOASchemaElement requestSchemaElement = _requestSchema.getRootElement();
-			
+
 			SOAElement requestElement = requestData.getRootElement();
-						
+
 			if (requestSchemaElement.getChildElements() != null) {
-										
+
 				for (int i = 0; i < requestSchemaElement.getChildElements().size(); i++) {
-					
+
 					SOASchemaElement childRequestSchemaElement = requestSchemaElement.getChildElements().get(i);
-					
+
 					SOAElement childRequestElement = requestElement.getChildElements().get(i);
-					
+
 					switch (childRequestSchemaElement.getDataType()) {
-						case SOASchema.INTEGER :							
+						case SOASchema.INTEGER :
 							parameters.add(Integer.parseInt(childRequestElement.getValue()));
-							break;							
-						case SOASchema.DECIMAL :							
-							parameters.add(Float.parseFloat(childRequestElement.getValue()));							
-							break;							
-						case SOASchema.DATE :	
+							break;
+						case SOASchema.DECIMAL :
+							parameters.add(Float.parseFloat(childRequestElement.getValue()));
+							break;
+						case SOASchema.DATE :
 							dateFormatter = rapidRequest.getRapidServlet().getXMLDateFormatter();
 							long dateLong = dateFormatter.parse(childRequestElement.getValue()).getTime();
 							date = new Date(dateLong);
 							parameters.add(date);
-							break;						
+							break;
 						case SOASchema.DATETIME :
 							dateFormatter = rapidRequest.getRapidServlet().getXMLDateTimeFormatter();
 							long dateTimeLong = dateFormatter.parse(childRequestElement.getValue()).getTime();
 							date = new Date(dateTimeLong);
 							parameters.add(date);
-							break;						
+							break;
 						default:
 							parameters.add(childRequestElement.getValue());
 					}
-					
+
 				}
-				
+
 			}
-									
-			// get the resultset!			
+
+			// get the resultset!
 			ResultSet rs = df.getPreparedResultSet(rapidRequest, _sql, parameters);
-									
+
 			// get the schema root
 			SOASchemaElement responseSchemaElement = _responseSchema.getRootElement();
-			
+
 			// get the response element
 			SOAElement responseElement = new SOAElement(responseSchemaElement.getName(), responseSchemaElement.getIsArray());
-															
+
 			while (rs.next()) {
-				
+
 				if (responseSchemaElement.getChildElements() != null) {
-					
+
 					for (SOASchemaElement responseChildElementSchema : responseSchemaElement.getChildElements()) {
-						
+
 						String elementName = responseChildElementSchema.getName();
-						
+
 						int elementType = responseChildElementSchema.getDataType();
-																
+
 						String fieldName = responseChildElementSchema.getField();
-						
+
 						String elementValue = null;
-																																				
+
 						switch (elementType) {
-							case SOASchema.INTEGER :							
-								elementValue = Integer.toString(rs.getInt(fieldName));							
-								break;							
-							case SOASchema.DECIMAL :							
-								elementValue = Float.toString(rs.getFloat(fieldName));							
-								break;							
-							case SOASchema.DATE :							
-								dateFormatter = rapidRequest.getRapidServlet().getXMLDateFormatter();							
-								date = rs.getDate(fieldName);							
-								if (date != null) elementValue = dateFormatter.format(date);							
-								break;							
-							case SOASchema.DATETIME :							
-								dateFormatter = rapidRequest.getRapidServlet().getXMLDateTimeFormatter();							
-								date = rs.getDate(fieldName);							
-								if (date != null) elementValue = dateFormatter.format(date);							
+							case SOASchema.INTEGER :
+								elementValue = Integer.toString(rs.getInt(fieldName));
+								break;
+							case SOASchema.DECIMAL :
+								elementValue = Float.toString(rs.getFloat(fieldName));
+								break;
+							case SOASchema.DATE :
+								dateFormatter = rapidRequest.getRapidServlet().getXMLDateFormatter();
+								date = rs.getDate(fieldName);
+								if (date != null) elementValue = dateFormatter.format(date);
+								break;
+							case SOASchema.DATETIME :
+								dateFormatter = rapidRequest.getRapidServlet().getXMLDateTimeFormatter();
+								Timestamp timeStamp = rs.getTimestamp(fieldName);
+								if (timeStamp != null) {
+									date = new Date(rs.getTimestamp(fieldName).getTime());
+									elementValue = dateFormatter.format(date);
+								}
 								break;
 							default:
 								elementValue = rs.getString(fieldName);
 						}
-						
+
 						// create a child response element
 						SOAElement responseChildElement = new SOAElement(elementName, elementValue);
-						
+
 						// add it (the method below knows what to do if the responseElement is an array or not)
 						responseElement.addChildElement(responseChildElement);
-						
+
 					}
-					
+
 				}
-																			
+
 				// check whether the parent element is an array
 				if (responseSchemaElement.getIsArray()) {
 					// if so close the array, which means further child elements are now added to a new parent element in the array collection
@@ -183,20 +187,20 @@ public class SQLWebservice extends Webservice {
 					// only use the first row in the record set if this is not an array
 					break;
 				}
-				
+
 			}
-					
+
 			SOAData responseData = new SOAData(responseElement);
-			
+
 			return responseData;
-			
+
 		} catch (Exception ex) {
-			
+
 			throw new WebserviceException(ex);
-			
+
 		}
-		
-		
+
+
 	}
-		
+
 }
