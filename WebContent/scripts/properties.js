04b5342cd054849e41c300a8a92d7f6e6a683845
Gareth Edwards <gareth.edwards@rapid-is.co.uk>
2017-08-13 19:21:44 +0100

    Form integration party types, removed person

--- WebContent/scripts/properties.js
+++ WebContent/scripts/properties.js
@@ -5277,7 +5277,7 @@ var _formObjects = {
 		"contact": {"name" : "Contact", "roles" : _formObjectRoles["PL"], "number" : true, "types" : [["","Please select..."],["email","Email address"],["mobile","Mobile number"],["phone","Phone number"],["home","Home number"],["work","Work number"]], "attributes" : [["value","Value"],["startDate","Start date"],["endDate","End date"]]},
 		"document": {"name" : "Document"},
 		"note": {"name" : "Note", "roles" : _formObjectRoles["CPL"], "types" : [["GENERAL","General"],["MEDICAL","Medical"]]},
-		"party": {"name" : "Party", "roles" : _formObjectRoles["PL"], "number" : true, "types": [["PERSON","Person"],["DOCTOR","Doctor"],["LANDLORD","Landlord"],["LETAGENT","Lettings agent"],["SOLICITOR","Solicitor"],["SUPTWORKER","Support worker"]], "attributes" : [["","Please select..."],["title","Title"],["forename","Forename"],["surname","Surname"],["dob","Date of birth"],["gender","Gender"],["ethnicity","Ethnicity"],["name","Organisation name"]]},
+		"party": {"name" : "Party", "roles" : _formObjectRoles["PL"], "number" : true, "types": [["PERSON","Person"],["DOCTOR","Doctor"],["LANDLORD","Landlord"],["LETAGENT","Lettings agent"],["SOLICITOR","Solicitor"],["SUPTWORKER","Support worker"],["OTHER","Other"]], "attributes" : [["","Please select..."],["title","Title"],["forename","Forename"],["surname","Surname"],["dob","Date of birth"],["gender","Gender"],["ethnicity","Ethnicity"],["name","Organisation name"]]},
 		"question": {"name" : "Question", "roles" : _formObjectRoles["CPL"], "number" : true}
 }
 
