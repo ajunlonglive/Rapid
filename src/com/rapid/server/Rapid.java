a6e1c07b8dc1d28405dc40fb947f6317544850f4
Mehmet Balande <mehmet.balande@rapid-is.co.uk>
2018-05-02 16:24:39 +0100

    File mime types can now have multiple file signatures
    It is now possible to have multiple file signatures for the same mime
    type in the web.xml

--- src/com/rapid/server/Rapid.java
+++ src/com/rapid/server/Rapid.java
@@ -1036,56 +1036,61 @@ public class Rapid extends RapidHttpServlet {
 
 									// check the content type is allowed
 									if (getUploadMimeTypes().contains(contentType)) {
-
+										
+										
 										// get the bytes
-										byte[] bytes = getUploadMimeTypeBytes().get(contentType);
+										List<byte[]> bytes = getUploadMimeTypeBytes().get(contentType);
 
 										// if we got some
 										if (bytes != null) {
-
-											// check the jpg, gif, png, bmp, or pdf file signature (from http://en.wikipedia.org/wiki/List_of_file_signatures)
-											if (Bytes.findPattern(bodyBytes, bytes, bytesOffset, bytes.length) > -1) {
-
-												try {
-
-													// create the path
-													String imagePath = "uploads/" +  app.getId() + "/" + imageName;
-													// servers with public access must use the secure upload location
-													if (this.isPublic()) imagePath = "WEB-INF/" + imagePath;
-													// create a file
-													File imageFile = new File(getServletContext().getRealPath(imagePath));
-													// create app folder if need be
-													imageFile.getParentFile().mkdir();
-													// create a file output stream to save the data to
-													FileOutputStream fos = new FileOutputStream(imageFile);
-													// write the body bytes to the stream
-													fos.write(bodyBytes, bytesOffset, bodyBytes.length - bytesOffset - boundary.length());
-													// close the stream
-													fos.close();
-
-													// log the file creation
-													logger.debug("Saved image file " + imagePath);
-
-													// print just the file name
-													out.print(imageFile.getName());
-
-													// close the writer
-													out.close();
-
-													// we passed the checks
-													passed = true;
-
-												} catch (Exception ex) {
-
-													// log
-													logger.error("Error saving uploaded file : " + ex.getMessage(), ex);
-
-													// rethrow
-													throw new Exception("Error uploading file");
-
-												}
-
-											} // signature check
+											//for each byte[] in the bytes list
+											for(int i = 0; i < bytes.size(); i++){
+												
+												// check the jpg, gif, png, bmp, or pdf file signature (from http://en.wikipedia.org/wiki/List_of_file_signatures)
+												if (Bytes.findPattern(bodyBytes, bytes.get(i), bytesOffset, bytes.get(i).length) > -1) {
+	
+													try {
+	
+														// create the path
+														String imagePath = "uploads/" +  app.getId() + "/" + imageName;
+														// servers with public access must use the secure upload location
+														if (this.isPublic()) imagePath = "WEB-INF/" + imagePath;
+														// create a file
+														File imageFile = new File(getServletContext().getRealPath(imagePath));
+														// create app folder if need be
+														imageFile.getParentFile().mkdir();
+														// create a file output stream to save the data to
+														FileOutputStream fos = new FileOutputStream(imageFile);
+														// write the body bytes to the stream
+														fos.write(bodyBytes, bytesOffset, bodyBytes.length - bytesOffset - boundary.length());
+														// close the stream
+														fos.close();
+	
+														// log the file creation
+														logger.debug("Saved image file " + imagePath);
+	
+														// print just the file name
+														out.print(imageFile.getName());
+	
+														// close the writer
+														out.close();
+	
+														// we passed the checks
+														passed = true;
+	
+													} catch (Exception ex) {
+	
+														// log
+														logger.error("Error saving uploaded file : " + ex.getMessage(), ex);
+	
+														// rethrow
+														throw new Exception("Error uploading file");
+	
+													}
+	
+												} // signature check
+											
+											} //end of bytes for
 
 										} // bytes check
 
