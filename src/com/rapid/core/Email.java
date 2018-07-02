d958a7ec41da0883d7b67a114be2c4acee32554e
Gareth Edwards <gareth.edwards@rapid-is.co.uk>
2018-07-02 11:04:41 +0100

    Trim on email host

--- src/com/rapid/core/Email.java
+++ src/com/rapid/core/Email.java
@@ -8,9 +8,9 @@ gareth.edwards@rapid-is.co.uk
 This file is part of the Rapid Application Platform
 
 Rapid is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as 
-published by the Free Software Foundation, either version 3 of the 
-License, or (at your option) any later version. The terms require you 
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version. The terms require you
 to include the original copyright, and the license notice in all redistributions.
 
 This program is distributed in the hope that it will be useful,
@@ -64,49 +64,50 @@ import com.rapid.utils.JAXB.EncryptedXmlAdapter;
 @XmlRootElement
 @XmlType(namespace="http://rapid-is.co.uk/core")
 public class Email {
-	
+
 	// public static classes
-	
+
 	public static class RapidMailAuthenticator extends javax.mail.Authenticator {
-    	
+
     	private String _userName, _password;
-    	
+
     	public RapidMailAuthenticator(String userName, String password) {
     		_userName = userName;
     		_password = password;
     	}
-    	
-    	protected PasswordAuthentication getPasswordAuthentication() {
+
+    	@Override
+		protected PasswordAuthentication getPasswordAuthentication() {
             return new PasswordAuthentication(_userName, _password);
         }
     }
-	
+
 	// attachments have a file name and a data source
 	public static class Attachment {
-		
+
 		private String _name;
 		private DataSource _dataSource;
-		
+
 		public Attachment(String name, DataSource dataSource) {
 			_name = name;
 			_dataSource = dataSource;
 		}
-		
+
 		public String getName() { return _name; }
 		public DataSource getDataSource() { return _dataSource; }
-		
+
 	}
-	
+
 	// useful for attachments created in memory from strings
 	public static class StringDataSource implements DataSource {
-		
+
 		private String _mimeType, _string;
-		
+
 		public StringDataSource(String mimeType, String string) {
 			_mimeType = mimeType;
 			_string = string;
 		}
-		
+
 		public StringDataSource(String string) {
 			this("text/plain",string);
 		}
@@ -130,39 +131,39 @@ public class Email {
 		public OutputStream getOutputStream() throws IOException {
 			return null;
 		}
-		
+
 	}
-	
+
 	// private static variables
 	private static Properties _properties;
 	private static Authenticator _authenticator;
 	private static Email _email;
-	
+
 	// private instance variables
 	private String _host, _security, _userName, _password;
 	private int _port;
 	private boolean _enabled;
-	
+
 	// properties
 	public boolean getEnabled() { return _enabled; }
 	public void setEnabled(boolean enabled) { _enabled = enabled; }
-	
+
 	public String getHost() { return _host; }
 	public void setHost(String host) { _host = host; }
-	
+
 	public int getPort() { return _port; }
 	public void setPort(int port) { _port = port; }
-	
+
 	public String getSecurity() { return _security; }
 	public void setSecurity(String security) { _security = security; }
-	
+
 	public String getUserName() { return _userName; }
 	public void setUserName(String userName) { _userName = userName; }
-	
+
 	@XmlJavaTypeAdapter( EncryptedXmlAdapter.class )
 	public String getPassword() { return _password; }
 	public void setPassword(String password) { _password = password; }
-	
+
 	// constructors
 	public Email() {}
 	public Email(String host, int port, String security, String userName, String password) {
@@ -172,38 +173,38 @@ public class Email {
 		_userName = userName;
 		_password = password;
 	}
-	
+
 	// instance methods
 	public void save(ServletContext servletContext) throws JAXBException, IOException {
-		
+
 		// get the file in which the control xml files are stored
 		File file = new File(servletContext.getRealPath("/WEB-INF/email/email.xml"));
 		// make dirs if need be
 		if (!file.exists()) file.getParentFile().mkdirs();
-		
+
 		// get the marshaller from the context
 		Marshaller marshaller = RapidHttpServlet.getMarshaller();
 		// marshall the devices to the file
 		marshaller.marshal(this, file);
-		
+
 		// retain this object in our static
         _email = this;
-        
+
 	}
-	
+
 	// static methods
     public static Email load(ServletContext servletContext) throws JAXBException, IOException {
-       
+
         // placeholder for the object
         Email email = null;
-       
+
         // get the file in which the email xml file is stored
         File file = new File(servletContext.getRealPath("/WEB-INF/email/email.xml"));
-       
+
         // if it exists
         if (file.exists()) {
             try {
-                // get the unmarshaller from the context 
+                // get the unmarshaller from the context
                 Unmarshaller unmarshaller = RapidHttpServlet.getUnmarshaller();
                 // unmarshall the devices
                 email = (Email) unmarshaller.unmarshal(file);
@@ -215,27 +216,27 @@ public class Email {
 
         // if we got one
         if (email != null) {
-                  
+
             // add email to the servlet context
             servletContext.setAttribute("email", email);
 
             // set the properties we've just loaded
-            setProperties(email.getHost(), email.getPort(), email.getSecurity(), email.getUserName(), email.getPassword());
-                        
+            setProperties(email.getHost().trim(), email.getPort(), email.getSecurity(), email.getUserName(), email.getPassword());
+
         }
-        
+
         // retain this object in our static
         _email = email;
-           
+
         // return the email object
         return email;
-       
+
     }
-    
+
     public static Email getEmailSettings() {
     	return _email;
     }
-    
+
     public static void setProperties(String host, int port, String security, String userName, String password) {
 
 	    // Get system properties
@@ -244,63 +245,64 @@ public class Email {
 	    // Setup mail server
 	    props.put("mail.smtp.host", host);
 	    props.put("mail.smtp.port", port);
-	    
+
 	    // check security - this article was very useful at describing the difference: https://luxsci.com/blog/ssl-versus-tls-whats-the-difference.html
 	    if ("ssl".equals(security)) {
-	    		    	
+
 	        props.put("mail.smtp.auth", "true");
 	        props.put("mail.smtp.ssl.enable", "true");
-	    	
+
 	    } else if ("tls".equals(security)) {
-	    
+
 	    	props.put("mail.smtp.auth", "true");
 		    props.put("mail.smtp.starttls.enable", "true");
 		    props.put("mail.smtp.socketFactory.fallback", "true");
-	    	
-	    } 
-	    
+
+	    }
+
 	    // retain these in a static
 	    _properties = props;
-	    
+
 	    // update the static object with these new values
     	_email = new Email(host, port, security, userName, password);
-	    
+
 	    // empty the authenticator so a new one is made on the first send
 	    _authenticator = null;
 
 	}
-    
+
     public static void send(String from, String to, String subject, String text, String html, Attachment... attachments) throws AddressException, MessagingException  {
-    	
+
     	// if email is null throw exception
     	if (_email == null) throw new MessagingException("Email settings must be specified");
-    	
+
     	// if properties are null
     	if (_properties == null) {
     		// set email properties from object properties
     		setProperties(_email.getHost(), _email.getPort(), _email.getSecurity(), _email.getUserName(), _email.getPassword());
     	}
-    	
+
 	    // if _authenticator is null
 	    if (_authenticator == null) {
 	    	// make a new one and save in static
 	    	_authenticator = new javax.mail.Authenticator() {
-	            protected PasswordAuthentication getPasswordAuthentication() {
+	            @Override
+				protected PasswordAuthentication getPasswordAuthentication() {
 	                return new PasswordAuthentication(_email.getUserName(), _email.getPassword());
 	            }
 	        };
 	    }
-	    
+
 	    // get the default session
     	Session session = Session.getInstance(_properties, _authenticator);
-    	
+
     	// create the message
     	Message message = new MimeMessage(session);
         message.setFrom(new InternetAddress(from));
         message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to.replace(";", ",")));
         message.setSubject(subject);
         message.setText(text);
-        
+
      // assume no attachments
         boolean gotAttachments = false;
         // if something was provided
@@ -309,24 +311,24 @@ public class Email {
             for (Attachment attachment : attachments) {
             	// if non- null
             	if (attachment != null) {
-            		// remember we have attachments            	
+            		// remember we have attachments
             		gotAttachments = true;
             		// we're done
             		break;
             	}
             }
         }
-        
+
         // if there were any attachments
         if (gotAttachments) {
-        	
+
         	// Create the message part
             BodyPart messageBodyPart = new MimeBodyPart();
 
             // set the text part
             messageBodyPart.setText(text);
-            
-            // add html if present 
+
+            // add html if present
             if (html != null) messageBodyPart.setContent(html, "text/html");
 
             // Create a multipart message
@@ -347,27 +349,27 @@ public class Email {
             }
             // add the complete message parts
             message.setContent(multipart);
-            
+
         } else {
-        	
+
         	// set message content type for html
         	if (html != null) message.setContent(html, "text/html");
-        	
+
         } // attachment check
 
         // send it!
         Transport.send(message);
-    	
+
     }
-    
+
     // overload to above for non-html
     public static void send(String from, String to, String subject, String text) throws AddressException, MessagingException {
     	send(from, to, subject, text, null, null);
     }
-    
+
     // overload to above for no attachments
     public static void send(String from, String to, String subject, String text, String html) throws AddressException, MessagingException {
     	send(from, to, subject, text, html, null);
     }
-	
+
 }
