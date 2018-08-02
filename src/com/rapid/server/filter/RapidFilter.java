9567676c0a45825afaf920697c82da54d8237324
Mehmet Balande <mehmet.balande@rapid-is.co.uk>
2018-08-02 11:05:44 +0100

    Pretty URLs can now be internally forwarded

--- src/com/rapid/server/filter/RapidFilter.java
+++ src/com/rapid/server/filter/RapidFilter.java
@@ -25,25 +25,35 @@ in a file named "COPYING".  If not, see <http://www.gnu.org/licenses/>.
 
 package com.rapid.server.filter;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.mysql.jdbc.StringUtils;
+import com.rapid.core.Application;
+import com.rapid.core.Applications;
 import com.rapid.utils.Classes;
 
 public class RapidFilter implements Filter {
 
-	// different applications' security adapters will retrieve different user objects
+	// different applications' security adapters will retrieve different user
+	// objects
 	public static final String SESSION_VARIABLE_INDEX_PATH = "index";
 	public static final String SESSION_VARIABLE_USER_NAME = "user";
 	public static final String SESSION_VARIABLE_USER_PASSWORD = "password";
@@ -54,6 +64,9 @@ public class RapidFilter implements Filter {
 
 	private RapidAuthenticationAdapter _authenticationAdapter;
 	private boolean _noCaching;
+
+	private Set<String> _resourceDirs = null;
+	private int _contextIdx = 1;		//keep track of the context position of the URL
 
 	// overrides
 
@@ -79,9 +92,12 @@ public class RapidFilter implements Filter {
 				// try and instantiate the authentication adapter
 				Class classClass = Class.forName(authenticationAdapterClass);
 				// check this class has the right super class
-				if (!Classes.extendsClass(classClass, com.rapid.server.filter.RapidAuthenticationAdapter.class)) throw new Exception(authenticationAdapterClass + " must extend com.rapid.server.filter.RapidAuthenticationAdapter.");
+				if (!Classes.extendsClass(classClass, com.rapid.server.filter.RapidAuthenticationAdapter.class))
+					throw new Exception(authenticationAdapterClass
+							+ " must extend com.rapid.server.filter.RapidAuthenticationAdapter.");
 				// instantiate an object and retain
-				_authenticationAdapter = (RapidAuthenticationAdapter) classClass.getConstructor(FilterConfig.class).newInstance(filterConfig);
+				_authenticationAdapter = (RapidAuthenticationAdapter) classClass.getConstructor(FilterConfig.class)
+						.newInstance(filterConfig);
 
 			}
 
@@ -95,12 +111,13 @@ public class RapidFilter implements Filter {
 	}
 
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+			throws IOException, ServletException {
 
 		_logger.trace("Process filter request...");
 
 		// fake slower responses like on mobile
-		//try { Thread.sleep(10000); } catch (InterruptedException e) {}
+		// try { Thread.sleep(10000); } catch (InterruptedException e) {}
 
 		// cast the request to http servlet
 		HttpServletRequest req = (HttpServletRequest) request;
@@ -112,18 +129,20 @@ public class RapidFilter implements Filter {
 		String ua = req.getHeader("User-Agent");
 
 		// if IE send X-UA-Compatible to prevent compatibility view
-		if (ua != null && ua.indexOf("MSIE") != -1) res.addHeader("X-UA-Compatible", "IE=edge,chrome=1");
+		if (ua != null && ua.indexOf("MSIE") != -1)
+			res.addHeader("X-UA-Compatible", "IE=edge,chrome=1");
 
 		// set all responses as UTF-8
 		response.setCharacterEncoding("utf-8");
 
 		// if no caching is on, try and prevent cache
-		if (_noCaching) noCache(res);
+		if (_noCaching)
+			noCache(res);
 
 		// assume this request requires authentication
 		boolean requiresAuthentication = true;
 
-		// get servlet path
+		// 1. Get the requested URI without the hostname -- gets servlet path
 		String path = req.getServletPath();
 
 		// all webservice related requests got to the soa servelet
@@ -140,7 +159,9 @@ public class RapidFilter implements Filter {
 					// put into lower case
 					contentType = contentType.toLowerCase();
 					// check this is known type of soa request xml
-					if (((req.getHeader("Action") != null || req.getHeader("SoapAction") != null) && contentType.contains("xml")) || (req.getHeader("Action") != null && contentType.contains("json"))) {
+					if (((req.getHeader("Action") != null || req.getHeader("SoapAction") != null)
+							&& contentType.contains("xml"))
+							|| (req.getHeader("Action") != null && contentType.contains("json"))) {
 						// remember we don't need authentication
 						requiresAuthentication = false;
 					}
@@ -149,25 +170,148 @@ public class RapidFilter implements Filter {
 		}
 
 		// online.htm doesn't need authentication
-		if ("/online.htm".equals(path)) requiresAuthentication = false;
+		if ("/online.htm".equals(path))
+			requiresAuthentication = false;
 
 		// safety doesn't need authentication
-		if ("/safety".equals(path)) requiresAuthentication = false;
+		if ("/safety".equals(path))
+			requiresAuthentication = false;
 
 		// if this request requires authentication
 		if (requiresAuthentication) {
 			// get a filtered request
 			ServletRequest filteredRequest = _authenticationAdapter.process(request, response);
+			String[] pathPart = (path.replaceFirst("/", "")).split("/");
+
 			// continue the rest of the chain with it if we got one
-			if (filteredRequest != null) filterChain.doFilter(filteredRequest, response);
-		} else {
+			if (filteredRequest != null) {
+
+				String rapidForwardURL;
+				Applications applications = (Applications) request.getServletContext().getAttribute("applications");
+				
+				
+				// if user has provided at least 1 path part (i.e. part1/) and the first part is a known application
+				if (pathPart.length >= 1 && applications.get(pathPart[0]) != null) {
+
+					// get the resource filenames only once
+					if (_resourceDirs == null) {
+						setResourceDirs(req);
+					}
+
+					String appID = pathPart[0];
+					rapidForwardURL = "/~?a=" + appID;
+					
+					if ("POST".equals(req.getMethod()) || ("GET".equals(req.getMethod()) && "~".equals(pathPart[pathPart.length - 1]))) {
+						rapidForwardURL = "/~?" + req.getQueryString();
+					
+
+					} else { //any other get requests
+
+						String version, page;
+						
+						switch (pathPart.length) {
+						case 1:	//if URL contains only the appID
+							rapidForwardURL = "/~?a=" + appID;
+							break;
+						
+						case 2:	//if URL contains appID/p or appID/v
+							// if what followed by appID is a Resource folder deal with it
+							if (isResource(pathPart[1])) {
+								rapidForwardURL = path.replaceFirst("/" + appID, "");
+					
+							} else { // otherwise it must be an application
+								
+								// Check whether 2nd part is a page or version only
+								if ("p".equalsIgnoreCase(String.valueOf(pathPart[1].charAt(0)))) {
+									page = pathPart[1];
+									rapidForwardURL += "&p=" + page;
+
+								} else {
+									version = pathPart[1];
+									rapidForwardURL += "&v=" + version;
+								}
+								
+								_contextIdx = 1;
+
+							}
+							break;
+							
+						default:	// if more than 2 parts
+							// check if this is a resource - remember the contextIdx position
+							if (isResource(pathPart[_contextIdx])) {
+								// restructure the url- without the applicationName/versionNumber
+								if (_contextIdx < 2) {
+									rapidForwardURL = path.replaceFirst("/" + appID, "");
+								} else {
+									rapidForwardURL = path.replaceFirst("/" + appID + "/" + pathPart[1], "");
+								}
+
+							} else { // otherwise it must be an application
+								version = pathPart[1];
+								page = pathPart[2];
+								rapidForwardURL += "&v=" + version + "&p=" + page;
+								_contextIdx = 2;
+							}
+							break;
+						}//end of switch
+						
+
+					}// end of if
+					
+					// forward the newly reconstructed URL
+					RequestDispatcher dispatcher = filteredRequest.getRequestDispatcher(rapidForwardURL);
+					dispatcher.forward(filteredRequest, response);
+					
+				} else {
+
+					// for regular rapid url formats
+					filterChain.doFilter(filteredRequest, response);
+
+				}
+
+			}
+
+		} else {// for requests not requiring authentication
 			// continue the rest of the chain
 			filterChain.doFilter(request, response);
 		}
 	}
 
+	// setter method
+	private void setResourceDirs(HttpServletRequest request) {
+
+		// check whether this is a RESOURCE or application request
+		String webContentString = request.getServletContext().getRealPath("/");
+		File webContentDir = new File(webContentString);
+		File[] directoryListing = webContentDir.listFiles();
+
+		_resourceDirs = new HashSet<String>();
+
+		// Loop over the root of the WebContent directory, to see whether
+		// appID/** is followed by one of the folders in this directory
+		// If so, then this request must be a RESOURCE--
+		if (directoryListing != null) {
+			for (File child : directoryListing) {
+				// if
+				if (child.isDirectory()) {
+					_resourceDirs.add(child.getName());
+				}
+			}
+		}
+	}
+
+	private boolean isResource(String fileName) {
+
+		if (_resourceDirs.contains(fileName)) {
+			return true;
+		}
+
+		return false;
+	}
+
 	@Override
-	public void destroy() {}
+	public void destroy() {
+	}
 
 	// public static method
 
