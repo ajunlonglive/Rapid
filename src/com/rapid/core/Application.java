db4ccd9f4834edd39c02f93edb9e264aeccc769a
Gareth Edwards <gareth.edwards@rapid-is.couk>
2018-08-08 06:29:51 +0100

    Fix to css scanning with spaces. About tab in Rapid Admin

--- src/com/rapid/core/Application.java
+++ src/com/rapid/core/Application.java
@@ -1100,6 +1100,8 @@ public class Application {
 						if (styleClass.indexOf(")") > 0) styleClass = styleClass.substring(0, styleClass.indexOf(")"));
 						// remove any colons
 						if (styleClass.indexOf(":") > 0) styleClass = styleClass.substring(0, styleClass.indexOf(":"));
+						// remove anything after a space
+						if (styleClass.indexOf(" ") > 0) styleClass = styleClass.substring(0, styleClass.indexOf(" "));
 						// check we don't have it already and add it if ok - i.e. contains no other .'s (this is common for urls getting picked up here)
 						if (!classes.contains(styleClass) && styleClass.indexOf(".") < 0) classes.add(styleClass);
 					}
