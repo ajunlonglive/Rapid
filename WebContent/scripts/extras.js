af7b09924ead269ede72102bd9468c93455ad5a9
jozef <jozef@lpt-win-07.rapid-is.office>
2019-12-23 17:04:10 +0000

    Allowed data copying empty gallery control to replace data store value with null.

--- WebContent/scripts/extras.js
+++ WebContent/scripts/extras.js
@@ -702,6 +702,9 @@ function makeDataObject(data, field) {
 			rows.push(row);
 		}
 		data = { fields: fields, rows: rows};
+	} else if (data === null) {
+		
+		data = { fields: [field], rows: [[null]]};
 	}
 	return data;
 }
@@ -732,7 +735,7 @@ function mergeDataObjects(data1, data2, mergeType, field, maxRows, details) {
 	var data = null;
 	if (data1) {
 		data1 = makeDataObject(data1);
-		if (data2) {
+		if (data2 !== undefined) {
 			data2 = makeDataObject(data2);
 			switch (mergeType) {
 				case "append" : case "row" :
