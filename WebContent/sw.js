8063b32c156b169ecb440ba782ded735ab99778a
Gareth Edwards <gareth.edwards@rapid-is.couk>
2020-06-01 14:39:59 +0100

    Small enhancement to sw for soa

--- WebContent/sw.js
+++ WebContent/sw.js
@@ -147,7 +147,7 @@ self.addEventListener("fetch", function(event) {
 	if (url.startsWith("rapid") || url.includes("a=rapid")) return;
 	
 	// proceed to direct server response  if request is for soa
-	if (url.startsWith("soa/")) return;
+	if (url.startsWith("soa")) return;
 	
 	// if request is for root or index
 	if (url === "" || url.endsWith("index.jsp")) {
