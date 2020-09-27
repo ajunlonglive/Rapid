8ef169efaf5f21dbb6ff54357dd0a6ee6c64cfd4
Gareth Edwards <gareth.edwards@rapid-is.couk>
2020-09-27 16:26:03 +0100

    Improvement to image upload to default to images on select

--- WebContent/scripts/properties.js
+++ WebContent/scripts/properties.js
@@ -1643,7 +1643,7 @@ function Property_imageFile(cell, propertyObject, property, details) {
 	}
 	
 	// append the  form control and the submit button
-	table.append("<tr><td><form id='form_" + propertyObject.id + "' method='post' enctype='multipart/form-data' target='uploadIFrame' action='designer?action=uploadImage&a=" + _version.id + "&v=" + _version.version + "&p=" + _page.id + "&c=" + propertyObject.id + "'><input id='file_" + propertyObject.id + "' name='file' type='file'></input></form></td></tr><tr><td><input type='submit' value='Upload' /></td></tr>");
+	table.append("<tr><td><form id='form_" + propertyObject.id + "' method='post' enctype='multipart/form-data' target='uploadIFrame' action='designer?action=uploadImage&a=" + _version.id + "&v=" + _version.version + "&p=" + _page.id + "&c=" + propertyObject.id + "'><input id='file_" + propertyObject.id + "' name='file' type='file' accept='image/*'></input></form></td></tr><tr><td><input type='submit' value='Upload' /></td></tr>");
 	
 	// get a reference to the submit button
 	addListener( table.find("input[type=submit]").click( {id : propertyObject.id}, function (ev) {
