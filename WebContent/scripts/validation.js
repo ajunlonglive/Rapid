2a3bc622e0b38aa3b359245000ae1a0dac101467
jozef <jozef@lpt-win-08.rapid-is.office>
2021-05-26 13:15:53 +0100

    Fixed validation of integers and added natural numbers

--- WebContent/scripts/validation.js
+++ WebContent/scripts/validation.js
@@ -28,7 +28,8 @@ var _validationTypes = [
   {value:'',text:'none',regex:''},                       
   {value:'value',text:'any value',regex:'[\\s\\S]'},
   {value:'number',text:'number',regex:'^[-]?\\d+\\.?\\d*$'},
-  {value:'integer',text:'integer',regex:'^\\d+$'},
+  {value:'integer',text:'integer',regex:'^-?\\d+$'},
+  {value:'natural',text:'natural',regex:'^\\d+$'},
   {value:'date',text:'date (dd/mm/yyyy)',regex:'^(((([1-9])|([0-2][0-9])|(3[01]))[\\/]((0[13578])|([13578])|(1[02])))|((([1-9])|([0-2][0-9])|(30))[\\/]((0[469])|([469])|(11)))|((([1-9])|([0-1][0-9]|2[0-8]))[\\/](2|02)))[\\/](\\d{2}){1,2}$|^(29\\/(2|02)\\/(19|20)?(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96))$'},
   {value:'dateUS',text:'date (mm/dd/yyyy)',regex:'^((((0[13578])|([13578])|(10)|(12))[\\/](([1-9])|([0-2][0-9])|(3[01]))|((0[469])|([469])|(11))[\\/]((1[02])|([1-9])|([0-2][0-9])|(30))|((2|02)[\\/](([1-9])|([0-1][0-9]|2[0-8]))))[\\/](\\d{2}){1,2})$|^((2|02)\\/29\\/(19|20)?(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96))$'},
   {value:'dateISO',text:'date (yyyy-mm-dd)',regex:'^(\\d{2}){1,2}[\\-](((0[13578])|([13578])|(10)|(12))[\\-](([1-9])|([0-2][0-9])|(3[01]))|(((0[469])|([469])|(11))[\\-](([1-9])|([0-2][0-9])|(30))|((2|02)[\\-](([1-9])|([0-1][0-9]|2[0-8]))))|((0[2])|(2))[\\-](([1-9])|([0-2][0-8])))$|^((19|20)?(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)\\-(2|02)\\-29)$'},
@@ -144,7 +145,7 @@ function showValidation(control) {
 				showValidation(_selectedControl);
 			}));	
 			// add the help listener
-			addHelp(control.id + "helpValType",true,true,"Choose how to apply the validation for this control. There are various patterns you can choose from to check the control value, or use logic, or even javascript, for more speicalised validation.");
+			addHelp(control.id + "helpValType",true,true,"Choose how to apply the validation for this control. There are various patterns you can choose from to check the control value, or use logic, or even javascript, for more specialised validation. Number allows any positive or negative number including decimal places, currency allow only 2 decimal places, integer does not allow decimals, and natural must be a positive integer");
 			// if the type is not "none"
 			if (type) {
 				switch (type) {
