6b88640c1287a742fd7d6e666cb9374737ef8754
steve <steve@localhost>
2022-06-22 11:29:51 +0100

    Added some string comparing routines and a substring delete method

--- src/com/rapid/utils/Strings.java
+++ src/com/rapid/utils/Strings.java
@@ -160,6 +160,74 @@ public class Strings {
 		return Integer.toString(n) + ordinalIndicator(n);
 	}
 
+	public static String deleteSubstring(String text, String textToDelete) {
+		
+		// check the location of one string in another
+		int location = text.indexOf(textToDelete);
+		
+		// if it contains the string then remove it
+		if(location>=0) {
+			text = text.substring(0, location) + text.substring(location + textToDelete.length());
+		}
+
+		return text;
+	}
+	
+	public static int editPercentage(String word1, String word2) {
+		
+		// get the longest of the two words
+		float maxWordLength = (word1.length()>word2.length())?word1.length():word2.length();
+
+		// get the edit distance of the two words
+		float editDistance = editDistance(word1, word2);
+
+		// now calculate the percentage difference
+		float percentageDifference = ((float)100/maxWordLength) * editDistance;
+
+		// return it
+		return (int)percentageDifference;
+	}
+	
+	public static int editDistance(String word1, String word2) {
+		int len1 = word1.length();
+		int len2 = word2.length();
+		
+		// len1+1, len2+1, because finally return dp[len1][len2]
+		int[][] dp = new int[len1 + 1][len2 + 1];
+		
+		for (int i = 0; i <= len1; i++) {
+			dp[i][0] = i;
+		}
+		
+		for (int j = 0; j <= len2; j++) {
+			dp[0][j] = j;
+		}
+		
+		//iterate though, and check last char
+		for (int i = 0; i < len1; i++) {
+			char c1 = word1.charAt(i);
+			for (int j = 0; j < len2; j++) {
+				char c2 = word2.charAt(j);
+				
+				//if last two chars equal
+				if (c1 == c2) {
+					//update dp value for +1 length
+					dp[i + 1][j + 1] = dp[i][j];
+				} else {
+					int replace = dp[i][j] + 1;
+					int insert = dp[i][j + 1] + 1;
+					int delete = dp[i + 1][j] + 1;
+					
+					int min = replace > insert ? insert : replace;
+					min = delete > min ? min : delete;
+					dp[i + 1][j + 1] = min;
+				}
+			}
+		}
+	 
+		return dp[len1][len2];
+	}
+
 	// Apache commons
 	// https://github.com/apache/commons-codec/blob/master/src/main/java/org/apache/commons/codec/binary/Hex.java
 
